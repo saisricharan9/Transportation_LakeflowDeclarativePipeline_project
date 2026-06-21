37f31e930e5bfb8eab90fed727685d33a2e6d440
saisricharan.9@gmail.com <saisricharan.9@gmail.com>
2026-06-21 07:15:58 +0000

    LakeFlow Declarative Pipeline of transportation project

---
+++ LakeFlow_Declarative_Pipeline_Transportation_project/transformations/bronze/city.py
@@ -0,0 +1,26 @@
+from pyspark import pipelines as dp
+from pyspark.sql.functions import col,current_timestamp
+from pyspark.sql.functions import md5, concat_ws, sha2
+
+
+SOURCE_PATH = "s3://transportation-prjt-1/datafolder/city"
+
+@dp.materialized_view(
+    name = "city",
+    comment = "City Raw Data processing",
+    table_properties = {
+        "quality":"bronze",
+        "layer":"bronze",
+        "source_format":"csv",
+        "delta.enableChangeDataFeed":"true",
+        "delta.autoOptimize.optimizeWrite":"true",
+        "delta.autoOptimize.autoCompact":"true"
+    }
+)
+
+def city_bronze():
+    df = spark.read.format("csv").option("header","true").option("inferSchema","true").option("mode","PERMISSIVE").option("mergeSchema","true").option("columnNameOfCorruptRecord","_corrupt_record").load(SOURCE_PATH)
+
+    df = df.withColumn("file_name",col("_metadata.file_path")).withColumn("ingest_datetime",current_timestamp())
+
+    return df
